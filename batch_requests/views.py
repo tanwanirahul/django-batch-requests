56923dda1ae075d990e7fc36f6598d71dfd9c8fa
David Percy <davetp425@gmail.com>
2015-11-05 15:27:01 -0500

    Fix import error for Django 1.4
    This should also work in newer versions (1.8) according to the docs:
    https://docs.djangoproject.com/en/1.8/ref/request-response/#usage

--- batch_requests/views.py
+++ batch_requests/views.py
@@ -7,7 +7,7 @@
 import json
 
 from django.core.urlresolvers import resolve
-from django.http.response import HttpResponse, HttpResponseBadRequest,\
+from django.http import HttpResponse, HttpResponseBadRequest,\
     HttpResponseServerError
 from django.template.response import ContentNotRenderedError
 from django.views.decorators.csrf import csrf_exempt
